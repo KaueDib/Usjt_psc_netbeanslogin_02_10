d34dec57fd07b58a9aa4435f805ecf6e3a02f67d
Kauê Dib <dib.kaue@gmail.com>
2023-10-02 20:58:31 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/dib/aula02_10_psc_sistema_academico/Aula02_10_psc_sistema_academico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.dib.aula02_10_psc_sistema_academico;
+
+/**
+ *
+ *@author 823149871
+ */
+public class Aula02_10_psc_sistema_academico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
